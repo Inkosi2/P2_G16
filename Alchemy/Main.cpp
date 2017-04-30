1710dc2df193ae5901405c5254276fd463a9fc5d
Oscar <oscarmasferrerrubio@enti.cat>
2017-04-30 23:23:17 +0200

    Principio  de  map

--- Alchemy/Main.cpp
+++ Alchemy/Main.cpp
@@ -1,23 +1,52 @@
 #include <istream>
+#include <ostream>
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <vector>
 #include <string>
 
-void main()
+int main()
 {
 	struct suma
 	{
 		std::string sum1;
 		std::string sum2;
 	};
-	std::map <std::string, suma> element;
 	
+	std::map <suma, std::string> element;
+	
+	std::string line;
+	std::string getKey;
+	std::string getSum1;
+	std::string getSum2;
+
+
+	// anterior código
+	/*std::ifstream fentrada;
+	fentrada.open("C:\\Users\Oscar\Desktop\elements.dat");
+
+	std::ostream fsalida("elements2.dat");
+
+	std::string line;
+
+	while (std::getline(fentrada, line))
+	{
+		
+		std::cout << line;
+	}
+
+	return 0;*/
+
 	std::ifstream fentrada("elements.dat");
 
-	char line[250];
-	while (fentrada.getline(line, 250))
+
+	while (!fentrada.eof)
 	{
-		std::cout << line << std::endl;
+		std::getline(fentrada, line);
+		
+		std::getc(line);
 	}
+
 }
+
