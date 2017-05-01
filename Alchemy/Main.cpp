f14045dbcb026b7f6efd153e5e06064ee17a4382
Oscar <oscarmasferrerrubio@enti.cat>
2017-05-02 01:40:30 +0200

    Corrección y más funciones

--- Alchemy/Main.cpp
+++ Alchemy/Main.cpp
@@ -24,126 +24,12 @@ struct pair_hash {
 std::vector<std::string> list({"Air", "Earth", "Fire", "Water"});
 std::vector<std::string> basics({ "Air", "Earth", "Fire", "Water" });
 
-int main()
-{
-	// Estructura para el mapa + el mapa
-	using suma = std::pair<std::string, std::string>;
+using suma = std::pair<std::string, std::string>;
 
-	std::unordered_map <suma, std::string, pair_hash> elementos;
-
-	// Elementos que se usan para leer "elements.dat"
-	std::string line;
-	std::string getResult;
-
-	// Variable para almacenar la entrada del jugador
-	std::string func;
-
-	// Uso en funciones
-	int aux;
-	int sum1, sum2;
-
-	// anterior código
-	/*std::ifstream fentrada;
-	fentrada.open("C:\\Users\Oscar\Desktop\elements.dat");
-
-	std::ostream fsalida("elements2.dat");
-
-	std::string line;
-
-	while (std::getline(fentrada, line))
-	{
-		
-		std::cout << line;
-	}
-
-	return 0;*/
-
-	std::ifstream fentrada;
-	
-	fentrada.open("elements.dat");
+std::unordered_map <suma, std::string, pair_hash> elementos;
 
 
 
-	while (getline(fentrada, line))
-	{
-		std::pair<std::string, std::string> suma;
-		
-		getResult = line.substr(0, line.find("="));
-		suma.first = line.substr(line.find("=") + 2, line.find("+") - line.find("=") - 3);
-		suma.second = line.substr(line.find("+") + 2, line.find("\n") - line.find("+"));
-		
-		elementos[suma] = getResult;
-
-		/*std::cout << line << std::endl;
-		while (getchar() != '=')
-		{
-			getSum1 += getchar();
-		}
-		while (getchar() != '+')
-		{
-			getSum2 += getchar();
-		}
-		while (getchar() != '\n')
-		{
-			getKey += getchar();
-		}
-		elements[{getSum1, getSum2}] = getKey;*/
-	}
-
-	fentrada.close();
-
-	//---------------------------------
-	//------ COUT de la interfaz ------
-	//---------------------------------
-
-	while (true)
-	{
-		std::cin >> func;
-		std::cout << std::endl;
-
-		if (func == "add %d") 
-		{
-			
-		}
-		
-		else if (func == "add basics")
-		{
-
-		}
-		
-		else if (func == "del")
-		{
-
-		}
-
-		/*else if (func == "info " + aux)
-		{
-
-		}*/
-		
-		else if (func == "sort")
-		{
-
-		}
-		
-		else if (func == "clean")
-		{
-
-		}
-		
-		else if (func == "help")
-		{
-
-		}
-		
-		/*else if (func == sum1 + " " + sum2)
-		{
-			
-		}*/
-	}
-
-
-}
 
 //enum funciones { add, add_basics, del, info, sort, clean, help};
 
@@ -154,15 +40,15 @@ void add(int elem)
 
 void add_basics()
 {
-	list.insert(list.end, basics[0]);
-	list.insert(list.end, basics[1]);
-	list.insert(list.end, basics[2]);
-	list.insert(list.end, basics[3]);
+	list.insert(list.end(), basics[1]);
+	list.insert(list.end(), basics[21]);
+	list.insert(list.end(), basics[3]);
+	list.insert(list.end(), basics[4]);
 }
 
 void del(int aux)
 {
-	list.erase(list.begin + aux -1);
+	list.erase(list.begin() + aux - 1);
 }
 
 void info(int elem)
@@ -175,10 +61,16 @@ void sort()
 	std::sort(list.begin(), list.end());
 }
 
-void clean()
-{
-
-}
+//void clean()
+//{
+//	for (int i = 0; i < list.size(); i++)
+//	{
+//		for (int j = i + 1; j < list.size; j++)
+//		{
+//
+//		}
+//	}
+//}
 
 void help()
 {
@@ -187,5 +79,105 @@ void help()
 
 void combine(int elem1, int elem2)
 {
+
+}
+
+void config()
+{
+	std::string line;
+	
+	std::ifstream fentrada;
+
+	fentrada.open("elements.dat");
+
+	std::string getResult;
+
+	while (getline(fentrada, line))
+	{
+		
+		suma key;
+
+		getResult = line.substr(0, line.find("="));
+		key.first = line.substr(line.find("=") + 2, line.find("+") - line.find("=") - 3);
+		key.second = line.substr(line.find("+") + 2, line.find("\n") - line.find("+"));
+
+		elementos[key] = getResult;
+	}
+
+	fentrada.close();
+}
+
+void print()
+{
+	for (int i = 0; i < list.size(); i++)
+	{
+		std::cout << i + 1 << "   " << list[i] << std::endl;
+	}
+}
+
+int main()
+{
+	// Variable para almacenar la entrada del jugador
+	std::string func;
+
+	// Uso en funciones
+	int aux;
+	int sum1, sum2;
+
+	config();
+	print();
+
+
+	//---------------------------------
+	//------ COUT de la interfaz ------
+	//---------------------------------
+
+	//while (true)
+	//{
+	//	std::cin >> func;
+	//	std::cout << std::endl;
+
+	//	if (func == "add %d") 
+	//	{
+	//		
+	//	}
+	//	
+	//	else if (func == "add basics")
+	//	{
+
+	//	}
+	//	
+	//	else if (func == "del")
+	//	{
+
+	//	}
+
+	//	/*else if (func == "info " + aux)
+	//	{
+
+	//	}*/
+	//	
+	//	else if (func == "sort")
+	//	{
+
+	//	}
+	//	
+	//	else if (func == "clean")
+	//	{
+
+	//	}
+	//	
+	//	else if (func == "help")
+	//	{
+
+	//	}
+	//	
+	//	/*else if (func == sum1 + " " + sum2)
+	//	{
+	//		
+	//	}*/
+	//}
+
+	return 0;
 
 }
